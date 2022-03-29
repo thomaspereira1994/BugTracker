105a9d6963da91929c02ff7b826982ff262f3c85
thomaspereira94 <thomaspereira1994@outlook.com>
2022-03-29 00:54:52 -0300

    Added Models
    Added Ticket, TicketAttachment, TicketComment, TicketHistory, TicketPriority, TicketStatus, TicketType.

---
+++ Models/TicketAttachment.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracker.Models
+{
+    public class TicketAttachment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("File Date")]
+        public DateTime Created { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        [DisplayName("File Description")]
+        public string Description { get; set; }
+
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile FormFile { get; set; }
+
+
+        [DisplayName("File Name")]
+        public string FileName { get; set; }
+        public byte[] FileData { get; set; }
+        
+
+        [DisplayName("File Extention")]
+        public string FileContentType { get; set; }
+
+
+        //Navigation properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; }
+    }
+}
---
+++ Models/TicketComment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BugTracker.Models
+{
+    public class TicketComment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Member Comment")]
+        public string Comment { get; set; }
+
+
+        [DisplayName("Date")]
+        public DateTime Created { get; set; }
+
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+
+        [DisplayName("Team Member")]
+        public int UserId { get; set; }
+
+
+        //Navigation properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; }
+
+
+    }
+}
